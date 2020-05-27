4146e2a0f1b5047fa0141dbd33a32d86f70f8f23
Hugo Lopez <hugo.lopez@bigcommerce.com>
2020-05-27 11:16:16 -0500

    Main.rake added to instance the Scraper class, the Task receive an String Valid URL

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -5,43 +5,60 @@ require 'pry'
 
 class Scraper
 
-    #BASE_PATH = "https://www.poderjudicialvirtual.com/"
-
-    #def initialize(url)
-    #    @url = url
-    #end
+    def initialize(url)
+        @url = url
+    end
 
     def parse_url(url)
         unparsed_page = open(url)
         Nokogiri::HTML(unparsed_page)
     end
 
-    def scraper(uri)
-        parsed_page = parse_url(uri)
+    def scraper
+        begin
+            parsed_page = parse_url(@url)
 
-        # header = parsed_page.css('div.header').children[1].text
-        # header_split = header.split('|')
-        # header_split_exp = header_split.last.split('Exp:')
+            # header = parsed_page.css('div.header').children[1].text
+            # header_split = header.split('|')
+            # header_split_exp = header_split.last.split('Exp:')
 
-        court = parsed_page.css('div.content').children[4].children[0].text.split('>').last.strip
-        author = parsed_page.css('div.content').children[4].children[2].text.split('Actor:').last.strip
-        defendant = parsed_page.css('div.content').children[4].children[4].text.split('Demandado:').last.strip
+            court = parsed_page.css('div.content').children[4].children[0].text.split('>').last.strip
+            author = parsed_page.css('div.content').children[4].children[2].text.split('Actor:').last.strip
+            defendant = parsed_page.css('div.content').children[4].children[4].text.split('Demandado:').last.strip
 
-        summary = parsed_page.css('div.content').children[7].text
-        notification = parsed_page.css('div.postDegradado').css('li.list-group-item').map{ |nots| p nots.children[3].text }.compact
+            summary = parsed_page.css('div.content').children[7].text
+            notification = parsed_page.css('div.postDegradado').css('li.list-group-item').map{ |nots| p nots.children[3].text }.compact
 
-        document_info = [
-            court: court,
-            author: author,
-            defendant: defendant,
-            summary: summary,
-            notification: notification
-        ]
+            document_info = [
+                court: court,
+                author: author,
+                defendant: defendant,
+                summary: summary,
+                notification: notification
+            ]
 
-        return document_info
+=begin
+            ## Array to test Create Document without the call
+            document_info = [
+                court: 'court',
+                author: 'author',
+                defendant: 'defendant',
+                summary: 'summary',
+                notification: 'notification'
+            ]
+=end
+
+            Document.create_from_collection(document_info)
+
+            print document_info
+            print '\nSave and Sound...'
+
+            return document_info
+
+        rescue => e
+            print "There was an error \n"
+            print e
+            Rails.logger.error("Error: Saving in DB. Error: #{e}")
+        end
     end
 end
-
-# scraper = Scraper.new('https://www.poderjudicialvirtual.com/df-floriberto-garcia-ramirez--c-director-del-registro')
-# scraper = Scraper.new("https://www.poderjudicialvirtual.com/df-trejo-bouquet-jacqueline-del-carmen--nueva-wal-mart-de-mexico-s-de-r-l-de-c-v-y-servicios-adminis-26/2020")
-# scraper.scraper
---
+++ lib/tasks/main.rake
@@ -0,0 +1,15 @@
+namespace :judicial do
+
+    desc 'Get info from /poderjudicial'
+  
+    task :scraper, [:url] => [:environment] do |_task, args|
+      return abort('Argument URL is required, must be a String valid URL') unless args.key?(:url)
+        print "Starting... \n"
+        
+        url = args[:url]
+        scraper = Scraper.new(url)
+        scraper.scraper
+
+        Rails.logger.info("Info: URL successful...")
+    end
+end
